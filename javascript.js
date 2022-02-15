f9cc26be0a70ddcc05104ce85da892b9a3ff2de6
Hamzaessa786 <98030939+Hamzaessa786@users.noreply.github.com>
2022-02-15 21:03:33 +0200

    Add files via upload

--- javascript.js
+++ javascript.js
@@ -1,98 +1,98 @@
-window.onload= beginningAnimation();
+window.onload = beginningAnimation();
 let computerSelection;
 let playerSelection;
-let computerscore= 0;
-let playerscore= 0;
+let computerScore = 0;
+let playerScore = 0;
 
-let buttons=document.querySelectorAll(".button");
-const body=document.querySelector("body");
-const main=document.querySelector("main");
-const endAlert=document.querySelector("#end-alert");
-const endDesc=document.querySelector("#end-desc");
-const returnMainBtn=document.querySelector("#retry-btn");
-const desc=document.querySelector("#desc3");
-const container=document.querySelector("#results-container");
+let buttons = document.querySelectorAll(".button");
+const body = document.querySelector("body");
+const main = document.querySelector("main");
+const endAlrt = document.querySelector("#end-alert");
+const endDesc = document.querySelector("#end-desc");
+const returnMainBtn = document.querySelector("#retry-btn");
+const desc = document.querySelector("#desc3");
+const container = document.querySelector("#results-container");
 
-body.addEventListener("click",skipAnime());
-body.addEventListener("keydown",skipAnime());
+body.addEventListener("click", skipAnime());
+body.addEventListener("keydown", skipAnime());
 
-function skipAnime(){
-    const span=document.querySelectorAll("span");
+function skipAnime() {
+  const span = document.querySelectorAll("span");
 
-    span.forEach((span)=>span.classlist.add("skip"));
+  span.forEach((span) => span.classList.add("skip"));
 }
 
-function beginningAnimation(){
-    fadein()
-     //need to turn nodelist of spans into an array so we can access last value for ontransitionend
-    const  desc1=document.querySelector("#Desc1");
-    let desc1Span=desc1.querySelectorAll("span");
+function beginningAnimation() {
+  fadeIn();
+  //need to turn nodelist of spans into an array so we can access last value for ontransitionend
+  const desc1 = document.querySelector("#desc1");
+  let desc1Span = desc1.querySelectorAll("span");
 
-desc1Span=Array.from(desc1Span);
+  desc1Span = Array.from(desc1Span);
 
-const desc2=document.querySelector("#Desc2");
-const desc3=document.querySelector("#Desc3");
+  const desc2 = document.querySelector("#desc2");
+  const desc3 = document.querySelector("#desc3");
 
-desc1Span[desc1Span.length-1].ontransitioned=()=>{
+  desc1Span[desc1Span.length - 1].ontransitionend = () => {
     desc1.classList.add("fade-out");
 
-    desc1.addEventListener("animationend",()=>{
-     desc1.classList.add("disappear");
-     desc1.classList.remove("animate");
-     desc2.classList.add("disappear");
-     desc2classList.remove("animate");
-     fadeIn();
-         /* need to collect nodelist of span 
+    desc1.addEventListener("animationend", () => {
+      desc1.classList.add("disappear");
+      desc1.classList.remove("animate");
+      desc2.classList.remove("disappear");
+      desc2.classList.add("animate");
+      fadeIn();
+      /* need to collect nodelist of span 
 in the same function we activate fadein()
 or else nodelist will be empty */
-let desc2Span=desc2.querySelector("Span");
-desc2Span=Array.from(desc2Span);
+      let desc2Span = desc2.querySelectorAll("span");
+      desc2Span = Array.from(desc2Span);
 
-desc2Span[desc2Span.length - 1].ontransitionend = () => {
-    desc2.classList.add("fade-out");
-    desc2.addEventListener("animationend", () => {
-      desc2.classList.add("disappear");
-      desc2.classList.remove("animate");
-      desc3.classList.remove("disappear");
-      desc3.classList.add("animate");
-      fadeIn();
+      desc2Span[desc2Span.length - 1].ontransitionend = () => {
+        desc2.classList.add("fade-out");
+        desc2.addEventListener("animationend", () => {
+          desc2.classList.add("disappear");
+          desc2.classList.remove("animate");
+          desc3.classList.remove("disappear");
+          desc3.classList.add("animate");
+          fadeIn();
 
-      let desc3Span = desc3.querySelectorAll("span");
+          let desc3Span = desc3.querySelectorAll("span");
           desc3Span = Array.from(desc3Span);
 
           desc3Span[desc3Span.length - 1].ontransitionend = () => {
             const cta = document.querySelector("#cta");
-  
-    cta.classList.add("drop-down");
-    
-    cta.addEventListener("animationend", () => {
-        const gameCtn = document.querySelector("#game-container");
 
-        setTimeout(function () {
-            gameCtn.classList.add("fade-in");
-          }, 300);
+            cta.classList.add("drop-down");
+
+            cta.addEventListener("animationend", () => {
+              const gameCtn = document.querySelector("#game-container");
+
+              setTimeout(function () {
+                gameCtn.classList.add("fade-in");
+              }, 300);
+            });
+          };
         });
       };
     });
   };
-});
-};
 }
- function fadeIn(){
-     let text=document.querySelector(".animate");
+function fadeIn() {
+  let text = document.querySelector(".animate");
 
-     let strtext=text.textContent;
-     let splittext=strtext.split("");
-     text.textcontent="";
-       //append span tags to each character in the string
+  let strText = text.textContent;
+  let splitText = strText.split("");
+  text.textContent = "";
+  //append span tags to each character in the string
   for (i = 0; i < splitText.length; i++) {
     text.innerHTML += `<span>${splitText[i]}</span>`;
   }
 
-  let char=0;
-  let timer=setInterval(onTick,50)
+  let char = 0;
+  let timer = setInterval(onTick, 50);
 
-    function onTick() {
+  function onTick() {
     const span = text.querySelectorAll("span")[char];
     span.classList.add("fade");
     char++;
@@ -287,4 +287,4 @@ function keepCpuScore() {
   });
 
   computerScoreBox.textContent = computerScore;
-} 
+}
